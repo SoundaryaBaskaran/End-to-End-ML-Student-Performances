1e0d5ca3c3f2f98c515ded417d97df540d7abd91
SoundaryaBaskaran <soundaryanimii@gmail.com>
2024-06-23 07:22:39 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name='MLPROJECT',
+    version='0.0.1',
+    author='Soundarya',
+    author_email='soundaryanimii@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas'
+
+    ]
+)
